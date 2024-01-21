2b47462b245bbb1330bff35ab74b24e7ac607c72
Christoffer Kragh Pedersen <ckp@kragh.dev>
2024-01-21 13:41:45 +0100

    Gotten a little further, but still not using correct metric functions

--- src/app.py
+++ src/app.py
@@ -1,11 +1,9 @@
 import os
-import logging
-from flask import Flask, Response
-from prometheus_client import Gauge, generate_latest
+from prometheus_client import start_http_server, Gauge
+import schedule
+import time
 from tenable.io import TenableIO
 from tenable.sc import TenableSC
-
-app = Flask(__name__)
 
 tio_variables = ['TIO_ACCESS_KEY', 'TIO_SECRET_KEY']
 sc_variables = ['SECURITYCENTER_NETWORK_ADDRESS', 'SC_USERNAME', 'SC_PASSWORD']
@@ -13,66 +11,58 @@ sc_variables = ['SECURITYCENTER_NETWORK_ADDRESS', 'SC_USERNAME', 'SC_PASSWORD']
 missing_tio_variables = [variable for variable in tio_variables if os.environ.get(variable) is None]
 missing_sc_variables = [variable for variable in sc_variables if os.environ.get(variable) is None]
 
-def enable_metrics():
-    if missing_tio_variables:
-        logging.info("Tenable Cloud metrics is enabled.")
-    if missing_sc_variables:
-        logging.info("Tenable Security Center metric is enabled.")
+# Create Prometheus metrics
+if not missing_tio_variables:
+    tenable_cloud_metrics = Gauge('tenable_cloud_metrics', 'Tenable Cloud Scans', ['id', 'uuid', 'name', 'status'])
+if not missing_sc_variables:
+    security_center_metrics = Gauge('security_center_metrics', 'Tenable Security Center Vulnerabilities', ['ip', 'pluginID', 'pluginName'])
 
-tenable_cloud_metrics = Gauge('tenable_cloud_metrics', 'Tenable Cloud Scans', ['id', 'uuid', 'name', 'status'])
-security_center_metrics = Gauge('security_center_metrics', 'Tenable Security Center Vulnerabilities', ['ip', 'pluginID', 'pluginName'])
 
-def verify_environment_variables():
-    if missing_tio_variables:
-        logging.warning(f"Missing environment variables for Tenable.io: {', '.join(missing_tio_variables)}")
+def get_environment_variable(variable):
+    return os.environ.get(variable).strip()
 
-    if missing_sc_variables:
-        logging.warning(f"Missing environment variables for Tenable.sc: {', '.join(missing_sc_variables)}")
+# Function to update metrics with new data
+def update_metrics():
+    if not missing_tio_variables:
+        tio_access_key = get_environment_variable('TIO_ACCESS_KEY')
+        tio_secret_key = get_environment_variable('TIO_SECRET_KEY')
+        report_tenable_cloud_metrics(tio_access_key, tio_secret_key)
 
-    if missing_tio_variables and missing_sc_variables:
-        logging.error("No environment variables for Tenable.io and Tenable.sc, nothing to scrape")
-        return False
-    return True
+    if not missing_sc_variables:
+        sc_network_address = get_environment_variable('SECURITYCENTER_NETWORK_ADDRESS')
+        sc_username = get_environment_variable('SC_USERNAME')
+        sc_password = get_environment_variable('SC_PASSWORD')
+        report_security_center_metrics(sc_network_address, sc_username, sc_password)
 
-def tenable_cloud_metrics(tio_access_key, tio_secret_key):
+def report_tenable_cloud_metrics(tio_access_key, tio_secret_key):
     # Tenable.io
     tio = TenableIO(tio_access_key, tio_secret_key)
-    for scan in tio.scans.list():
-        tenable_cloud_metrics.labels(id=scan['id'], uuid=scan['uuid'], name=scan['name'], status=scan['status']).set(1)
+    for vuln in tio.exports.vulns():
+        print("Vuln was {}".format(vuln))
+        tenable_cloud_metrics.labels(vuln)
 
-def security_center_metrics(sc_network_address, sc_username, sc_password):
+def report_security_center_metrics(sc_network_address, sc_username, sc_password):
     # Tenable.sc
     sc = TenableSC(sc_network_address)
     sc.login(sc_username, sc_password)
     for vuln in sc.analysis.vulns():
-        security_center_metrics.labels(ip=vuln['ip'], pluginID=vuln['pluginID'], pluginName=vuln['pluginName']).set(1)
+        security_center_metrics.info({
+            'ip':vuln['ip'],
+            'pluginID': vuln['pluginID'],
+            'pluginName': vuln['pluginName']
+        })
 
-@app.route('/metrics')
-def metrics():
-    if not verify_environment_variables():
-        return Response("ERROR: Missing environment variables", status=500)
+# Schedule the update_metrics function to run every 5 minutes
+schedule.every(1).seconds.do(update_metrics)
 
-    enable_metrics()
-
-    if not missing_tio_variables:
-        tio_access_key = os.environ['TIO_ACCESS_KEY']
-        tio_secret_key = os.environ['TIO_SECRET_KEY']
-        tenable_cloud_metrics(tio_access_key, tio_secret_key)
-
-    if not missing_sc_variables:
-        sc_network_address = os.environ['SECURITYCENTER_NETWORK_ADDRESS']
-        sc_username = os.environ['SC_USERNAME']
-        sc_password = os.environ['SC_PASSWORD']
-        security_center_metrics(sc_network_address, sc_username, sc_password)
-    return Response(generate_latest(), mimetype='text/plain')
-
-@app.route('/healthz')
-def healthz():
-    if not verify_environment_variables():
-        return Response("ERROR: Missing environment variables", status=500)
-    return Response("OK", status=200)
-
-
+# Start the Prometheus HTTP server
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, port=8080)
+    # Start Prometheus HTTP server on port 8000
+    start_http_server(8080)
+
+    while True:
+        # Run pending scheduled tasks
+        schedule.run_pending()
+
+        # Sleep for 1 second to avoid excessive CPU usage
+        time.sleep(1)
