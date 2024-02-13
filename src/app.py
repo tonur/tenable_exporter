c605f80b09c83e41805b6737213f4690fd0c61c7
Christoffer Kragh Pedersen <ckp@kragh.dev>
2024-02-13 14:23:42 +0100

    Begining of aggregation and argument parser

--- src/app.py
+++ src/app.py
@@ -1,55 +1,81 @@
 from prometheus_client import start_http_server, Gauge
 from tenable.io import TenableIO
+import argparse
 import logging
-import os
 import time
-import signal
 
-def signal_handler(sig, frame):
-    logging.info("Received termination signal. Exiting gracefully.")
-    exit(0)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Tenable exporter for Prometheus. Provide your Access Key and Secret Key to scrape to expose Prometheus metrics.")
+    parser.add_argument("--tenable.access_key", required=True, help="Tenable Access Key")
+    parser.add_argument("--tenable.secret_key", required=True, help="Tenable Secret Key")
+    parser.add_argument("--tenable.interval", "-i", default=600, type=int, help="Polling interval for requesting data from Tenable API in seconds")
+    parser.add_argument("--tenable.timeout", default=30, type=int, help="Timeout for requests against Tenable API")
+    parser.add_argument("--web.listen_address", default=":9521", help="Address on which to expose metrics")
+    # Add other flags as needed
+    args = parser.parse_args()
 
-signal.signal(signal.SIGINT, signal_handler)
+    # Set up logging
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
 
 # Create Prometheus metrics
 vulnerability_metric = Gauge('tenable_vulnerability_info', 'Tenable Vulnerability Information',
                              ['asset_uuid', 'plugin_id', 'plugin_name', 'severity', 'state', 'source'])
 
-def get_environment_variable(variable):
-    return os.environ.get(variable).strip()
-
 def fetch_tenable_data(tio_access_key, tio_secret_key):
     try:
         tio = TenableIO(tio_access_key, tio_secret_key)
 
-        for vuln in tio.exports.vulns():
-            asset = vuln.get('asset', {})
-            plugin = vuln.get('plugin', {})
+        vulnerabilities = tio.exports.vulns()
+        for vulnerability in vulnerabilities:
+            asset = vulnerability.get('asset', {})
+            plugin = vulnerability.get('plugin', {})
 
             vulnerability_metric.labels(
                 asset_uuid=asset.get('uuid', ''),
                 plugin_id=plugin.get('id', ''),
                 plugin_name=plugin.get('name', ''),
-                severity=vuln.get('severity', ''),
-                state=vuln.get('state', ''),
-                source=vuln.get('source', '')
+                severity=vulnerability.get('severity', ''),
+                state=vulnerability.get('state', ''),
+                source=vulnerability.get('source', '')
             ).set(1)
 
     except Exception as e:
         logging.exception(f"Error fetching Tenable data: {e}")
 
+def aggregation_key(vulnerability):
+    asset = vulnerability.get('asset', {})
+    plugin = vulnerability.get('plugin', {})
+    return f"{asset.get('uuid', '')}_{plugin.get('id', '')}_{plugin.get('name', '')}_{vulnerability.get('severity', '')}_{vulnerability.get('state', '')}_{vulnerability.get('source', '')}"
+
+def aggregate_vulnerabilities(vulnerabilities):
+    aggregate_results = {}
+    logging.debug(f"Input vulnerabilities to aggregate was: {vulnerabilities}")
+    for vulnerability in vulnerabilities:
+        asset = vulnerability.get('asset', {})
+        plugin = vulnerability.get('plugin', {})
+        key = aggregation_key(vulnerability)
+        if key not in aggregate_results:
+            aggregate_results[key] = {
+                'uuid': asset.get('uuid', ''),
+                'id': plugin.get('id', ''),
+                'name': plugin.get('name', ''),
+                'severity': vulnerability.get('severity', ''),
+                'state': vulnerability.get('state', ''),
+                'source': vulnerability.get('source', ''),
+            }
+        aggregate_results[key]['count'] += 1
+        logging.debug(f"Added aggregation for vulnerability{vulnerability} with key {key}")
+
+    output = list(aggregate_results.values())
+    logging.debug(f"Output of aggregation was: {output}")
+    return output
+
 def update_metrics():
-    tio_access_key = get_environment_variable('TIO_ACCESS_KEY')
-    tio_secret_key = get_environment_variable('TIO_SECRET_KEY')
-
-    if not tio_access_key or not tio_secret_key:
-        logging.error('Environment variables TIO_ACCESS_KEY and TIO_SECRET_KEY must be set')
-        raise ValueError('Missing TIO_ACCESS_KEY or TIO_SECRET_KEY')
-
-    fetch_tenable_data(tio_access_key, tio_secret_key)
+    fetch_tenable_data(args.tenable.access_key, args.tenable.secret_key)
 
 update_interval_seconds = 300
-start_http_server(8080)
+start_http_server(args.tenable.listen_address)
 
 while True:
     update_metrics()
