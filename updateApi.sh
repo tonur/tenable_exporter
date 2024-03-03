6a26a2596f4667b3ac396413d14132658c2725fd
Christoffer Kragh Pedersen <ckp@kragh.dev>
2024-03-03 18:54:14 +0100

    Update openapi generator update script

--- updateApi.sh
+++ updateApi.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i " https://developer.tenable.com/openapi/653bf52677b3bd00411bda79" -g rust -o out/rust
+docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i "https://developer.tenable.com/openapi/653bf52677b3bd00411bda79" -g rust -o /local/openapi_generator/rust
