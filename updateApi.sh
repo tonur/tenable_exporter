eef95412d33d425132d73ff18c64c3208e9d594b
Christoffer Kragh Pedersen <ckp@kragh.dev>
2024-03-03 20:22:39 +0100

    Generate as user:group

--- updateApi.sh
+++ updateApi.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i "https://developer.tenable.com/openapi/653bf52677b3bd00411bda79" -g rust -o /local/openapi_generator/rust
+docker run --rm -v ${PWD}:/local --user $UID:$GID openapitools/openapi-generator-cli generate -i "https://developer.tenable.com/openapi/653bf52677b3bd00411bda79" -g rust -o /local/openapi_generator/rust
